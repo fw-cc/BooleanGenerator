a82948ae442973e9b44cf2cbf37053c6385b68d8
Whole-of-Europe <bae.gregs.base@gmail.com>
2019-10-06 16:53:27 +0100

    Removed coro version of boolean generation.

--- core.py
+++ core.py
@@ -3,34 +3,6 @@ import sys
 import os
 
 
-async def generate_bool_coro(n_must_match, list_of_terms):
-    while True:
-        try:
-            list_of_terms.pop(list_of_terms.index(["EMPTY", "EMPTY"]))
-        except ValueError:
-            break
-    formatted_list_of_terms = []
-    for item in list_of_terms:
-        formatted_list_of_terms.append(item[1].get())
-    output_listed = list(itertools_combinations(formatted_list_of_terms, n_must_match))
-
-    formatted_string = ""
-
-    for perm_iter in range(len(output_listed)):
-        formatted_string += "("
-        for term_iter in range(len(output_listed[perm_iter])):
-            if output_listed[perm_iter][term_iter] != "":
-                formatted_string += output_listed[perm_iter][term_iter]
-                if term_iter != len(output_listed[perm_iter]) - 1:
-                    formatted_string += " AND "
-        if perm_iter != len(output_listed) - 1:
-            formatted_string += ") OR "
-        else:
-            formatted_string += ")"
-
-    return formatted_string
-
-
 def resource_path(relative_path):
     """ Get absolute path to resource, works for dev and for PyInstaller """
     try:
@@ -50,7 +22,8 @@ def generate_bool(n_must_match, list_of_terms):
             break
     formatted_list_of_terms = []
     for item in list_of_terms:
-        formatted_list_of_terms.append(item[1].get())
+        if item[1].get() != "":
+            formatted_list_of_terms.append(item[1].get())
     output_listed = list(itertools_combinations(formatted_list_of_terms, n_must_match))
 
     formatted_string = ""
@@ -72,6 +45,6 @@ def generate_bool(n_must_match, list_of_terms):
 
 if __name__ == "__main__":
     assert sys.version_info >= (3, 7), "Minimum Python version: 3.7.0"
-    print(generate_bool_coro(3, [["wadwadwadw", "test1"],
+    print(generate_bool(3, [["wadwadwadw", "test1"],
                                  ["wewawwefwf", "test2"],
                                  ["wadwafeawd", "test3"]]))
